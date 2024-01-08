341608c3e78aa450cf238fc4ecb9735213feddf7
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-01-09 02:47:31 +0700

    update utils

---
+++ utils.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import tensorflow.layers as layers
+
+
+def text_from_ids(ids):
+    return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
+
+
+vocab = ['\n', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+         'v', 'w', 'x', 'y', 'z']
+
+ids_from_chars = layers.StringLookup(
+    vocabulary=list(vocab), mask_token=None)
+
+chars_from_ids = layers.StringLookup(
+    vocabulary=list(vocab), invert=True, mask_token=None)
