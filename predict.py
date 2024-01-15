2785eadcf4be123e1a426a3c83a95cbc8f35babb
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-01-15 13:37:01 +0700

    generate new name from a char/str

---
+++ predict.py
@@ -0,0 +1,27 @@
+import argparse
+from utils import *
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inputs', type=str)
+    parser.add_argument('--save_path', type=str, default="./weight/model_v1.keras")
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    inputs = args.inputs
+
+    generator = tf.keras.models.load_model(args.save_path)
+    result = generator.predict(inputs)
+
+    name = extract_name(result)
+
+    return name
+
+
+if __name__ == "__main__":
+    main()
