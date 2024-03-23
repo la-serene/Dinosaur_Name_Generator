700a4719d305b148c1fd9d8a1a56ffbe9df1aa67
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-03-23 21:09:42 +0700

    add train and train_step fn

--- train.py
+++ train.py
@@ -8,6 +8,21 @@ from model import Generator
 from utils import *
 
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--training', type=str, default="./data/dino.txt")
+    parser.add_argument('--pretrain', type=str, default="./data/pretrain")
+    parser.add_argument('--hidden_units', type=int, default=128)
+    parser.add_argument('--embedding_size', type=int, default=256)
+    parser.add_argument('--epochs', type=int, default=40)
+    parser.add_argument('--pretrain_epochs', type=int, default=40)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--learning_rate', type=float, default=0.001)
+    parser.add_argument('--save_path', type=str, default="./weights/model_v2.h5")
+
+    return parser.parse_args()
+
+
 @tf.function
 def train_step(x, y,
                model,
@@ -45,38 +60,20 @@ def train(model,
             print(f"Epoch: {epoch + 1}, loss = {loss}")
 
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--training', type=str, default="./data/dino.txt")
-    parser.add_argument('--pretrain', type=str, default="./data/pretrain")
-    parser.add_argument('--hidden_units', type=int, default=128)
-    parser.add_argument('--embedding_size', type=int, default=256)
-    parser.add_argument('--epochs', type=int, default=40)
-    parser.add_argument('--pretrain_epochs', type=int, default=40)
-    parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--learning_rate', type=float, default=0.001)
-    parser.add_argument('--save_path', type=str, default="./weights/model_v2.h5")
-
-    return parser.parse_args()
-
-
 def main():
     args = get_args()
 
-    vocab_size = len(vocab) + 1
-    generator = Generator(vocab_size, args.embedding_size, args.hidden_units)
+    vocab_size = len(vocab)
+    generator = Generator(vocab_size, args.hidden_units, args.embedding_size)
 
     dataset = get_training(args.training, 1024, args.batch_size)
     pretrain = get_pretrain(args.pretrain, 1024, args.batch_size)
 
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     optimizer = tf.keras.optimizers.Adam(args.learning_rate)
 
-    # Pretraining on the Vertebrata name pretrain
-    generator.train(pretrain, loss_fn, optimizer, epochs=args.pretrain_epochs)
-
-    # Training on the Dinosaur name pretrain
-    generator.train(dataset, loss_fn, optimizer, epochs=args.epochs)
+    train(generator, pretrain, loss_fn, optimizer, epochs=args.pretrain_epochs)
+    train(generator, dataset, loss_fn, optimizer, epochs=args.epochs)
 
     generator.save_weights(args.save_path)
 
